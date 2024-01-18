18e48bee0125ae5e7bdc919dbf5eb8d1a10175b6
Tanner Spendlove <tannerhs25@gmail.com>
2024-01-17 17:25:37 -0700

    fixed numbering for positions to be 1,1 to 8,8

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,6 +7,7 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    //be careful, only the boardlength for a 0-indexed array; starting at 1,
     public static final int BOARD_LENGTH=8;
     private ChessPiece[][] board = new ChessPiece[BOARD_LENGTH][BOARD_LENGTH];  //make and initialize 2-d array
     public ChessBoard() {
@@ -19,10 +20,9 @@ public class ChessBoard {
      * @param position where to add the piece to
      * @param piece    the piece to add
      */
-    public void addPiece(ChessPosition position, ChessPiece piece) {  //FIXME ide says board is never written to
+    public void addPiece(ChessPosition position, ChessPiece piece) {  //input positions start at 1,1 and are not 0-indexed
 
-        //board[0][0]=new ChessPiece(piece.getTeamColor(),piece.getPieceType());
-        board[position.getRow()][position.getColumn()]=piece;
+        board[(position.getRow()-1)][(position.getColumn()-1)]=piece;
     }
 
     /**
@@ -32,8 +32,9 @@ public class ChessBoard {
      * @return Either the piece at the position, or null if no piece is at that
      * position
      */
-    public ChessPiece getPiece(ChessPosition position) {
-        return board[position.getRow()][position.getColumn()];
+    public ChessPiece getPiece(ChessPosition position) {  //array holding pieces is 0-indexed, but ChessPositions are not
+
+        return board[position.getRow()-1][position.getColumn()-1];
     }
 
     /**
@@ -41,6 +42,19 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        board = new ChessPiece[BOARD_LENGTH][BOARD_LENGTH];  //is this acceptable with java's automatic garbage collection or should I clear each element from the 2d array?
+        for(int r=0; r<BOARD_LENGTH; r++) {
+            for(int c=0; c<BOARD_LENGTH;c++) {
+                board[r][c]=null;
+            }
+        }
+        //set pieces at starting positions
+        setStartingPieces();
+        return;
+    }
+
+    public void setStartingPieces(){  //hardcode starting values; check to see if white or black should be at bottom of board
+        ChessPosition currentPosition=new ChessPosition(8,8);
+        addPiece(currentPosition,new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.ROOK));
+
     }
 }
