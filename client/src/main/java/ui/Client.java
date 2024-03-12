8e224de21ccd4ceb736275dbb651c1a118b35f6f
tannerhs <tannerhs25@gmail.com>
2024-03-12 12:19:16 -0600

    started phase 5

---
+++ client/src/main/java/ui/Client.java
@@ -0,0 +1,12 @@
+package ui;
+
+import chess.ChessGame;
+
+public class Client {
+
+    public void drawChessBoard(ChessGame gameIn) {
+        //draw headers
+        //draw rows and cols
+    }
+
+}
---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,12 @@
+package ui;
+
+public class ServerFacade {  //calls your server
+
+    public LoginResponse login(LoginRequest request) {
+        return null;
+    }
+
+    public RegisterRequest register(RegisterRequest request) {
+        return null;
+    }
+}
