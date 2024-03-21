6962befa9dfb8f4a337650cfb23c468a0505e879
tannerhs <tannerhs25@gmail.com>
2024-03-20 23:26:09 -0600

    changed test cases to run on randomly assigned port

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -17,9 +17,9 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(8080);
+        var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
-        facade = new ServerFacade(8080);
+        facade = new ServerFacade(port);
         System.out.println("Started test HTTP client on " + port);
 
     }
